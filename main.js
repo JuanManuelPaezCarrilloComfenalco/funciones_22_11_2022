ece91571100d607ee797819446f4f42733e37130
JuanManuelPaezCarrilloComfenalco <paez3939@gmail.com>
2022-11-24 03:21:30 -0500

    Funcion Arrow

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 addEventListener("DOMContentLoaded", (e)=>{
+// Arrow functions: son funciones flecha o «fat arrow» son una forma corta de escribir 
+// funciones que aparece en Javascript a partir de ECMAScript 6. Básicamente,
+// se trata de reemplazar eliminar la palabra function y añadir => antes de abrir las llaves.
+// las funciones flechas tienen algunas ventajas a la hora de simplificar
+// código bastante interesantes: 
+// Si el cuerpo de la función sólo tiene una línea, podemos omitir las llaves ({}).
+// Además, en ese caso, automáticamente se hace un return de esa única línea, por lo que podemos omitir también el return.
+// En el caso de que la función no tenga parámetros, se indica como en el ejemplo anterior: () =>.
+// En el caso de que la función tenga un solo parámetro, se puede indicar simplemente el nombre del mismo: e =>.
+// En el caso de que la función tenga 2 ó más parámetros, se indican entre paréntesis: (a, b) =>.
+// Si queremos devolver un objeto, que coincide con la sintaxis de las llaves, se puede englobar con paréntesis: ({name: 'Manz'}).
+const func = function () {
+  return "Función tradicional.";
+};
 
-// Clasulas: una clausura o cierre se define como una función que «encierra» variables en su
-// propio ámbito(y que continúan existiendo aún habiendo terminado la función).
-// Ejemplo:
+const func = () => {
+  return "Función flecha.";
+};
 
-// Clausura: Función incr()
-const incr = (function () {
-  let num = 0;
-  return function () {
-    num++;
-    return num;
-  };
-})();
+// o se podria simplificar de la esta manera:
 
-typeof incr; // 'function'
-incr(); // 1
-incr(); // 2
-incr(); // 3
+const func = () => "Función flecha."; // 0 parámetros: Devuelve "Función flecha"
+const func = (e) => e + 1; // 1 parámetro: Devuelve el valor de e + 1
+const func = (a, b) => a + b; // 2 parámetros: Devuelve el valor de a + b
+
 })
-// 
+ 
