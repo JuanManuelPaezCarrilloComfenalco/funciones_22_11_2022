209bfc1bb407451859cd3448c69dca17e1c94307
JuanManuelPaezCarrilloComfenalco <paez3939@gmail.com>
2022-11-24 01:59:55 -0500

    funciones por expresion

---
+++ Funiones por expresion/main.js
@@ -0,0 +1,11 @@
+addEventListener("DOMContentLoaded", (e)=>{
+    // consiste en crear funciones en el interior de una variable
+    // Ejemplo: El segundo "saludar" (nombre de la función) se suele omitir: es redundante
+    
+const saludo = function saludar() {
+    return "Hola";
+  };
+  
+  saludo(); // 'Hola'
+    
+})
